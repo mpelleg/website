8d7a9fbbcad77cb3a746ed1ac45b57e000aa2f49
Matteo Pellegri <mpellegri199@gmail.com>
2020-09-12 10:43:05 +0200

    Update sketch.js
    Just some changes

--- Tutorials/P5JS/p5.js/08/8.01_p5.js_What_is_HTML/sketch.js
+++ Tutorials/P5JS/p5.js/08/8.01_p5.js_What_is_HTML/sketch.js
@@ -2,6 +2,11 @@
 
 function setup() {
   createCanvas(200, 200);
+  createElement('h1','Ma porco dio')
+}
+
+function mousePressed(params) {
+  createP("Porco dio is " + random(0,10))
 }
 
 function draw() {
